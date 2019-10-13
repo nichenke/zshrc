7ff736aa007f6530ed5732e1c865146ee5de37db
Nic Henke <nichenke@gmail.com>
2019-10-13 17:40:20 -0600

    Add first config
    This is the start of everything.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+brew install zsh
+
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
